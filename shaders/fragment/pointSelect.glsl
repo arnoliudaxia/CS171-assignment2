e651c25e26fb475ff0a7f42f407805ec58dea367
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-10-15 19:04:56 +0800

    优化UI

--- shaders/fragment/pointSelect.glsl
+++ shaders/fragment/pointSelect.glsl
@@ -5,13 +5,13 @@ in vec3 normalV;
 uniform vec3 selection;
 
 void main(){
-if(normalV==selection)
-{
-color = vec4(.968f,.745f,0.094f,1.f);
+	if(normalV==selection)
+	{
+	color = vec4(.968f,.745f,0.094f,1.f);
 
-}
-else
-{
-color = vec4(.5f,.5f,.5f,0.1f);
-}
+	}
+	else
+	{
+	color = vec4(.5f,.5f,.5f,0.3f);
+	}
 }
